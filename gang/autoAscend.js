c2c93da8c8751586cfeb865f95e5bb6413924e2e
Sauleline <135928366+Sauleline@users.noreply.github.com>
2024-02-12 13:28:45 +1030

    Update autoAscend.js

--- gang/autoAscend.js
+++ gang/autoAscend.js
@@ -10,8 +10,13 @@ function customRound(a, b) {
 	let dp = 10 ** b
 	return (Math.trunc(a * dp) / dp)
 }
-
 export async function main(ns) {
+	ns.disableLog("ALL")
+
+	ns.tail()
+
+	ns.clearLog()
+
 	//Settings
 	let tick = 1000
 	//time between checks (in ms)
@@ -20,12 +25,18 @@ export async function main(ns) {
 
 	let members = ns.gang.getMemberNames()
 	let results = {}
+	let hac, str, def, dex, agi, cha = ""
+
+	ns.setTitle("Hypothetical Ascention Results")
 
 	while (true) {
+		ns.print("| member name |  hac  |  str  |  def  |  dex  |  agi  |  cha  |")
+		ns.print("+-------------+-------+-------+-------+-------+-------+-------+")
 		for (let i = 0; i < members.length; i++) {
 			results = ns.gang.getAscensionResult(members[i])
+
 			if (results.str >= threshold) {
-				/////////^ YOU CAN CHANGE THIS IF YOU WANT TO CHECK A DIFFERENT STAT
+				//				 ^ YOU CAN CHANGE THIS IF YOU WANT TO CHECK A DIFFERENT STAT
 				results = (ns.gang.ascendMember(members[i]))
 				ns.tprint(members[i] + " has been ascended.")
 				ns.tprint("You have lost:")
@@ -38,7 +49,96 @@ export async function main(ns) {
 				ns.tprint("×" + customRound(results.agi, 2) + " Agility multiplier")
 				ns.tprint("×" + customRound(results.cha, 2) + " Charisma multiplier")
 			}
+			else {
+				hac = String(customRound(results.hack, 2))
+				str = String(customRound(results.str, 2))
+				def = String(customRound(results.def, 2))
+				dex = String(customRound(results.dex, 2))
+				agi = String(customRound(results.agi, 2))
+				cha = String(customRound(results.cha, 2))
+
+				if (hac.length == 1) {
+					hac = (" ×" + hac + "  ")
+				}
+				else if (str.length == 2) {
+					hac = (" ×" + hac + " ")
+				}
+				else if (str.length == 3) {
+					hac = ("×" + hac + " ")
+				}
+				else{
+					hac = ("×" + hac)
+				}
+
+				if (str.length == 1) {
+					str = (" ×" + str + "  ")
+				}
+				else if (str.length == 2) {
+					str = (" ×" + str + " ")
+				}
+				else if (str.length == 3) {
+					str = ("×" + str + " ")
+				}
+				else{
+					str = ("×" + str)
+				}
+
+				if (def.length == 1) {
+					def = (" ×" + def + "  ")
+				}
+				else if (def.length == 2) {
+					def = (" ×" + def + " ")
+				}
+				else if (def.length == 3) {
+					def = ("×" + def + " ")
+				}
+				else{
+					def = ("×" + def)
+				}
+
+				if (dex.length == 1) {
+					dex = (" ×" + dex + "  ")
+				}
+				else if (str.length == 2) {
+					dex = (" ×" + dex + " ")
+				}
+				else if (str.length == 3) {
+					dex = ("×" + dex + " ")
+				}
+				else{
+					dex = ("×" + dex)
+				}
+
+				if (agi.length == 1) {
+					agi = (" ×" + agi + "  ")
+				}
+				else if (agi.length == 2) {
+					agi = (" ×" + agi + " ")
+				}
+				else if (agi.length == 3) {
+					agi = ("×" + agi + " ")
+				}
+				else{
+					agi = ("×" + agi)
+				}
+
+				if (cha.length == 1) {
+					cha = (" ×" + cha + "  ")
+				}
+				else if (cha.length == 2) {
+					cha = (" ×" + cha + " ")
+				}
+				else if (cha.length == 3) {
+					cha = ("×" + cha + " ")
+				}
+				else{
+					cha = ("×" + cha)
+				}
+
+				ns.print("| " + members[i] + " | " + hac + " | " + str + " | " + def + " | " + dex + " | " + agi + " | " + cha + " | ")
+			}
 		}
 		await ns.sleep(tick)
+		ns.clearLog()
 	}
 }
