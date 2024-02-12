bd2d418551151219110aef873e3b09c1db972c4e
Sauleline <135928366+Sauleline@users.noreply.github.com>
2024-02-12 14:16:46 +1030

    Update renameMembers.js

--- gang/renameMembers.js
+++ gang/renameMembers.js
@@ -6,11 +6,29 @@ export async function main(ns) {
 	let members = ns.gang.getMemberNames()
 	let name = ""
 	let original = ""
-	for (let i = 0; i < members.length; i++){
-		name = ns.args[0]
-		original = members[i]
-		name = name.concat(i)
-		ns.gang.renameMember(members[i], name)
-		ns.tprint(original + " is now " + name)
+	for (let i = 0; i < members.length; i++) {
+		if (Math.floor((i+1) / 10) == 0) {
+			name = ns.args[0]
+			original = members[i]
+			name = name.concat((i + 1) * 10)
+			ns.gang.renameMember(members[i], name)
+			ns.tprint(original + " is now " + name)
+		}
+
+		else if (i == 9) {
+			name = ns.args[0]
+			original = members[i]
+			name = name.concat(i + 2)
+			ns.gang.renameMember(members[i], name)
+			ns.tprint(original + " is now " + name)
+		}
+
+		else {
+			name = ns.args[0]
+			original = members[i]
+			name = name.concat((i + 3))
+			ns.gang.renameMember(members[i], name)
+			ns.tprint(original + " is now " + name)
+		}
 	}
 }
