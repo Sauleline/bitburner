5aa7b1ff486c56ee8a91e23e859b1485b4d19a19
Peony <135928366+Sauleline@users.noreply.github.com>
2024-05-22 11:59:18 +0930

    Update lastFM.js

--- UI/lastFM.js
+++ UI/lastFM.js
@@ -1,13 +1,22 @@
 /** @param {NS} ns */
 //just a little script i made to show me what song im listening to at the moment. there are simple instructions for how to get a lastfm api key online, use them.
 export async function main(ns) {
+	function lfm(func, key, ...params) {
+		let out = "https://ws.audioscrobbler.com/2.0/?method=" + func + "&api_key=" + key
+		for (let i = 0; i < params.length; i++) {
+			out = out + "&" + params[i][0] + "=" + params[i][1]
+		}
+		out = out + "&format=json"
+		return (out)
+	}
+
 	ns.tail()
 	ns.disableLog("ALL")
 	let username = "YOUR LASTFM USERNAME"
 	let apiKey = "YOUR API KEY"
 	while (true) {
-		let response = await fetch("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+username+"&api_key="+apiKey+"&format=json&limit=1&extended=1");
-		let recent = await response.json();
+		let response = await fetch(lfm("user.getrecenttracks", apiKey, ["user", username], ["limit", 1], ["extended", 1]))
+		let recent = await response.json()
 		ns.clearLog()
 		let recentTrack = recent.recenttracks.track[0]
 		ns.print("Now Playing: " + recentTrack.name)
